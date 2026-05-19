6f723c3ba0c1bcbb50dddbbcf2d344de6287f373
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2026-05-19 16:34:21 +0100

    Fix add_to_cart issue

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get existing items from localstorage or initialize to an empty array
+  const cartItems = getLocalStorage("so-cart") || [];
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
