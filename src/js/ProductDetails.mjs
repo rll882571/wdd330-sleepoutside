d24e3799b32cb9f525af0eebb09b54f42c3f31c7
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2026-05-20 07:47:52 +0100

    Create dynamic product detail pages

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,52 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+
+  async init() {
+    // Fetch the product details
+    this.product = await this.dataSource.findProductById(this.productId);
+
+    // Render the HTML
+    this.renderProductDetails();
+
+    // Handle addToCart click event
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+
+  addProductToCart() {
+    // Get existing items from localstorage or initialize to an empty array
+    const cartItems = getLocalStorage("so-cart") || [];
+    cartItems.push(this.product);
+    setLocalStorage("so-cart", cartItems);
+  }
+
+  renderProductDetails() {
+    productDetailsTemplate(this.product);
+  }
+}
+
+function productDetailsTemplate(product) {
+  document.querySelector("h2").textContent = product.Brand.Name;
+  document.querySelector("h3").textContent = product.NameWithoutBrand;
+
+  document.title = `Sleep Outside | ${product.Brand.Name}`;
+
+  const productImage = document.getElementById("productImage");
+  productImage.src = product.Image;
+  productImage.alt = product.NameWithoutBrand;
+
+  document.getElementById("productPrice").textContent = product.FinalPrice;
+  document.getElementById("productColor").textContent =
+    product.Colors[0].ColorName;
+  document.getElementById("productDesc").innerHTML =
+    product.DescriptionHtmlSimple;
+
+  document.getElementById("addToCart").dataset.id = product.Id;
+}
