8cfe466ffd7297a395bc9286a5dce7fa36f4dc4b
rfl882571 <rfl882571@gmail.com>
2026-05-28 10:54:11 -0300

    f

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,10 +2,10 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
   return `<li class="product-card">
-    <a href="product_pages/?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.Name}">
-      <h2 class="card__brand">${product.Brand.Name}</h2>
-      <h3 class="card__name">${product.Name}</h3>
+    <a href="../product_pages/index.html?product=${product.Id}">
+      <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p>
     </a>
   </li>`;
@@ -19,8 +19,9 @@ export default class ProductList {
   }
 
   async init() {
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
     this.renderList(list);
+    document.querySelector(".title").innerHTML = this.category;
   }
 
   renderList(list) {
