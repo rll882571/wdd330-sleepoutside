b1a953b6d3356046a70be0096cf2926ab85f11ac
rfl882571 <rfl882571@gmail.com>
2026-05-28 14:36:39 -0300

    c

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,4 @@
-import { updateCartCount } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
-<<<<<<< HEAD
-import { loadHeaderFooter, getParam } from "./utils.mjs";
+// Carrega os componentes modulares e o contador do carrinho
 loadHeaderFooter();
-
-
-// 2. Captura a categoria real vinda da URL (ex: tents, backpacks, etc.)
-const category = getParam("category");
-
-// 3. Passa a categoria dinâmica para os nossos motores
-const dataSource = new ProductData(); // O construtor agora é limpo, lembra?
-const element = document.querySelector(".product-list");
-
-// 4. Cria a lista passando a categoria dinâmica capturada da URL
-const productList = new ProductList(category, dataSource, element);
-
-
-productList.init();
-
-=======
-updateCartCount();
->>>>>>> rl--individual3
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,13 +1,16 @@
-
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
 
-const textoDaUrl = window.location.search;
-const construtorParametros = new URLSearchParams(textoDaUrl);
-const category = construtorParametros.get('category');
-const dataSource = new ProductData(category);
-const element = document.querySelector('.product-list');
+// Carrega o cabeçalho e rodapé nesta página também
+loadHeaderFooter();
+
+// Captura a categoria da URL (ex: tents)
+const category = getParam("category");
+
+const dataSource = new ProductData();
+const element = document.querySelector(".product-list");
+
+// Inicializa a lista dinâmica com a categoria capturada
 const productList = new ProductList(category, dataSource, element);
-
-
-productList.init(); 
+productList.init();
